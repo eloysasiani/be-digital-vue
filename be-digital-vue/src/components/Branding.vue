3afad3859b194605211d14887e307b101673f9ed
eloysasiani <sateliseloysa@gmail.com>
2025-07-28 20:45:59 +0100

    Adicionamos toda estrutura e html do Nav, Branding, Header e headerPrincipal

--- be-digital-vue/src/components/Branding.vue
+++ be-digital-vue/src/components/Branding.vue
@@ -3,14 +3,17 @@
         <p class="brand">Algumas empresas que confiam em nossos serviços:</p>
         <div class="container">
             <img :src="spotify" alt="">
-            <img src="./assets/images/slack.svg" alt="">
-            <img src="./assets/images/dropbox.svg" alt="">
-            <img src="./assets/images/zoom.svg" alt="">
+            <img :src="slack" alt="">
+            <img :src="dropbox" alt="">
+            <img :src="zoom" alt="">
         </div>
     </section>
 </template>
 <script setup>
 import spotify from "@/assets/images/spotify.svg"
+import dropbox from "@/assets/images/dropbox.svg"
+import slack from "@/assets/images/slack.svg" 
+import zoom from "@/assets/images/zoom.svg"
 </script>
 <style scoped>
 </style>
