792f81a108cfa52e8612f78b2b6e845aeb39bdff
eloysasiani <sateliseloysa@gmail.com>
2025-07-31 14:50:54 +0100

    Mudanças no branding e adição, começo modificações article

--- be-digital-vue/src/components/Article.vue
+++ be-digital-vue/src/components/Article.vue
@@ -1,20 +1,25 @@
 <template>
        <div class="cards">
-              <!-- Seta com Content-->
-              <img :src="businessDeal" alt="">
+
               <div class="conteudo">
-                     <h2>Branding & Design system</h2>
-                     <p>Normalmente usado em indústrias gráficas e produtoras para pré-visualização de designs, layouts
-                            e mockups</p>
-                     <svg width="48" height="40" viewBox="0 0 24 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M23.2493 8.70711C23.6398 8.31658 23.6398 7.68342 23.2493 7.29289L16.8853 0.928932C16.4948 0.538408 15.8616 0.538408 15.4711 0.928932C15.0806 1.31946 15.0806 1.95262 15.4711 2.34315L21.1279 8L15.4711 13.6569C15.0806 14.0474 15.0806 14.6805 15.4711 15.0711C15.8616 15.4616 16.4948 15.4616 16.8853 15.0711L23.2493 8.70711ZM0.313232 9H22.5421V7H0.313232V9Z"
-                                   fill="#272D38" />
-                     </svg>
+                     <img :src="businessDeal" alt="">
+                     <div class="text">
+                            <h2>Branding & Design system</h2>
+                            <p>Normalmente usado em indústrias gráficas e produtoras para pré-visualização de designs,
+                                   layouts
+                                   e mockups</p>
+                            <svg width="48" height="40" viewBox="0 0 24 16" fill="none"
+                                   xmlns="http://www.w3.org/2000/svg">
+                                   <path d="M23.2493 8.70711C23.6398 8.31658 23.6398 7.68342 23.2493 7.29289L16.8853 0.928932C16.4948 0.538408 15.8616 0.538408 15.4711 0.928932C15.0806 1.31946 15.0806 1.95262 15.4711 2.34315L21.1279 8L15.4711 13.6569C15.0806 14.0474 15.0806 14.6805 15.4711 15.0711C15.8616 15.4616 16.4948 15.4616 16.8853 15.0711L23.2493 8.70711ZM0.313232 9H22.5421V7H0.313232V9Z"
+                                          fill="#272D38" />
+                            </svg>
+                     </div>
+
               </div>
 
-              <div class="cards">
-                     <!-- Seta com Content-->
-                     <div class="conteudo">
+
+              <div class="conteudo">
+                     <div class="text">
                             <h2>Análise de resultados</h2>
                             <p>Todo projeto de comunicação precisa de um objetivo bem definido. Ajudamos no
                                    acompanhamento dos resultados obtidos.</p>
@@ -23,8 +28,9 @@
                                    <path d="M23.2493 8.70711C23.6398 8.31658 23.6398 7.68342 23.2493 7.29289L16.8853 0.928932C16.4948 0.538408 15.8616 0.538408 15.4711 0.928932C15.0806 1.31946 15.0806 1.95262 15.4711 2.34315L21.1279 8L15.4711 13.6569C15.0806 14.0474 15.0806 14.6805 15.4711 15.0711C15.8616 15.4616 16.4948 15.4616 16.8853 15.0711L23.2493 8.70711ZM0.313232 9H22.5421V7H0.313232V9Z"
                                           fill="#272D38" />
                             </svg>
-                            <img :src="businessManCheckingFinancialReport" alt="">
                      </div>
+                     <img :src="businessManCheckingFinancialReport" alt="">
+
               </div>
 
        </div>
@@ -33,4 +39,31 @@
 import businessManCheckingFinancialReport from "@/assets/images/businessman-checking-financial-report.png"
 import businessDeal from "@/assets/images/business-deal.png"
 </script>
-<style scoped></style>
+<style scoped>
+.cards {
+       display: grid;
+       grid-template-columns: 1fr;
+       grid-gap: 20px;
+       grid-template-rows: repeat(2, 1fr);
+}
+
+.conteudo {
+       display: grid;
+       grid-template-columns: repeat(2, 1fr);
+}
+
+.text {
+       display: flex;
+       flex-direction: column;
+}
+
+.text h2 {
+       background: linear-gradient(90deg, rgba(131, 58, 180, 1) 0%, rgba(253, 29, 29, 1) 90%);
+       -webkit-background-clip: text;
+       background-clip: text;
+       color: transparent;
+       -webkit-text-fill-color: transparent;
+       font-size: 5rem;
+
+}
+</style>
--- be-digital-vue/src/components/Branding.vue
+++ be-digital-vue/src/components/Branding.vue
@@ -8,23 +8,84 @@
             <img :src="zoom" alt="">
         </div>
     </section>
+
+    <section class="more-about-us">
+        <div>
+            <img :src="trophy" alt="" class="firstimage">
+            <h2 id="counter-text">Mais de 0 prêmios</h2>
+        </div>
+        <div>
+            <img :src="threestar" alt="" class="secondimage">
+            <h2 id="counter2-text">Mais de 0 clientes satisfeitos</h2>
+        </div>
+        <div>
+            <img :src="star" alt="" class="otherimage">
+            <h2 id="counter3-text">Mais de 0 vistorias</h2>
+        </div>
+    </section>
 </template>
+
 <script setup>
+import { onMounted } from 'vue'
 import spotify from "@/assets/images/spotify.svg"
 import dropbox from "@/assets/images/dropbox.svg"
 import slack from "@/assets/images/slack.svg"
 import zoom from "@/assets/images/zoom.svg"
+import star from "@/assets/images/star.png"
+import trophy from "@/assets/images/gold-thropy.png"
+import threestar from "@/assets/images/three-star-rating.png"
+
+onMounted(() => {
+    const animateCounter = (id, limit, text) => {
+        let count = 1
+        const el = document.getElementById(id)
+        const interval = setInterval(() => {
+            el.textContent = `Mais de ${count} ${text}`
+            count++
+            if (count > limit) clearInterval(interval)
+        }, 100)
+    }
+
+    animateCounter("counter-text", 25, "prêmios")
+    animateCounter("counter2-text", 62, "clientes satisfeitos")
+    animateCounter("counter3-text", 47, "vistorias")
+})
 </script>
+
 <style scoped>
 .container {
-    display: grid;
-    gap: 2rem;
-    grid-template-rows: 1fr;
-    grid-template-columns: repeat(4, 1fr);
+    display: flex;
+    gap: 10rem;
+    justify-content: space-around;
+    padding: 2rem;
+    border-radius: 1rem;
+    background: linear-gradient(90deg, rgba(131, 58, 180, 1) 0%, rgb(173, 10, 10) 90%);
 }
 
 .branding {
     display: grid;
     gap: 4rem;
+    background: linear-gradient(90deg, rgba(2, 0, 36, 1) 0%, rgba(9, 9, 121, 1) 35%, rgba(0, 212, 255, 1) 100%);
+    padding: 4rem;
+    color: white;
+    border-radius: 2rem;
+}
+
+.more-about-us img {
+    width: 12vh;
+}
+
+.more-about-us h2 {
+    font-size: 1.4rem;
+}
+
+.more-about-us {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    background: #2A7B9B;
+    background: #2A7B9B;
+    background: linear-gradient(90deg, rgba(42, 123, 155, 1) 0%, rgba(8, 53, 117, 1) 50%, rgba(111, 237, 83, 1) 100%);
+
 }
 </style>
